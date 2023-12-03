4df937b5a10f2bc2a94e1c4b75a3cbc880483b13
danielgoss22 <daniel.goss22@gmail.com>
2023-12-02 17:02:25 -0700

    set up mostly working timer

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,7 @@ var questionsE1 = document.getElementById("questions");
 var quizEndE1 = document.getElementById("quiz-end");
 var timerE1 = document.getElementById("time-allowed");
 var scoreE1 = document.getElementById("score");
+var startBtn = document.querySelector(".start");
 // var startBtn = document.querySelector(".start");
 var timer;
 var timeAllowed;
@@ -59,7 +60,8 @@ function getQuestion() {
       btnEl.addEventListener("click", function () {
         if (this.textContent !== currentQuestion.rightAns) {
           //TODO: subtract 3 seconds from the timer
-          //timeCounter -3
+          //timeCounter -3\
+          timeAllowed -= 5;
 
           currentIndex++;
           getQuestion();
@@ -82,47 +84,50 @@ function getQuestion() {
 
 function quizEnd() {
   var quizEndE2 = document.createElement("h1");
-  // scoreE1 = document.createElement("h3");
 
   quizEndE2.textContent = "End Of Quiz!";
   quizEndE1.appendChild(quizEndE2);
   scoreE1.textContent = "Your Score " + scoreE2[scoreE2.length - 1];
-  // scoreE1.appendChild(scoreE2);
+
   // [{ javier: 1 }]; local storage stringify with JSONor{ "javier": 1 }
 }
 // console.log(this);
-getQuestion();
-// function startQuiz() {
-//   isCorrect = false;
-//   timeAllowed = 80;
-//   startBtn.disabled = true;
-//   getQuestion();
-//   startTimer();
-// }
-console.log(scoreE2);
+// getQuestion();
+
+function startQuiz() {
+  // isCorrect = false;
+  timeAllowed = 30;
+  if (startBtn === false) {
+  }
+
+  getQuestion();
+  startTimer();
+}
+
 function startTimer() {
   // Sets timer
   timer = setInterval(function () {
     timeAllowed--;
-    timerE1.textContent = timeAllowed;
-    if (timeAllowed >= 0) {
-      // Tests if win condition is met
-      if (isWin && timeAllowed > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
+    timerE1.textContent = "Time left " + timeAllowed;
+    // if (timeAllowed >= 0) {
+    //   // Tests if win condition is met
+    //   if (isWin && timeAllowed > 0) {
+    //     // Clears interval and stops timer
+    //     clearInterval(timer);
+    //     winGame();
+    //   }
+    // }
     // Tests if time has run out
-    if (timerCount === 0) {
+    if (timeAllowed === 0) {
       // Clears interval
       clearInterval(timer);
-      loseGame();
+      quizEnd();
     }
   }, 1000);
 }
+startBtn.addEventListener("click", startQuiz);
 // questionTitle.textContent = quiz[1].question;
 // questionsE1.appendChild(question1);
 // function quizQ1() {}
 // console.log(quiz.question);
-// s;
+// s
